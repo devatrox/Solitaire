318fcaeb6da1c0d029cf6dd5c528d0732d818dcf
Jason Nickel <jni@vnr.de>
2020-01-19 15:33:04 +0100

    Removed old stuff in reducer; Linter fixes

--- src/reducer.ts
+++ src/reducer.ts
@@ -1,6 +1,4 @@
 import _filter from 'lodash/filter';
-import _shuffle from 'lodash/shuffle';
-import _isArray from 'lodash/isArray';
 import _find from 'lodash/find';
 import _last from 'lodash/last';
 import _cloneDeep from 'lodash/cloneDeep';
@@ -15,22 +13,15 @@ const reducer = (prevState: AppState, action: Action): AppState => {
     if (type === ActionTypes.MOVE_CARDS && payload.source && payload.target) {
         const [sourceName, sourceIndex, sourceCards] = payload.source;
         const [targetName, targetIndex] = payload.target;
-        console.log('MOVE_CARDS', sourceName, targetName, sourceCards);
 
         const newSource = _cloneDeep(prevState[sourceName]);
 
         const newTarget = _cloneDeep(prevState[targetName]);
 
-        if (_isArray(sourceCards)) {
-            for (const sourceCard of sourceCards) {
-                newSource[sourceIndex] = _filter(newSource[sourceIndex], (card) => card.id !== sourceCard.id);
+        for (const sourceCard of sourceCards) {
+            newSource[sourceIndex] = _filter(newSource[sourceIndex], (card) => card.id !== sourceCard.id);
 
-                newTarget[targetIndex].push(sourceCard);
-            }
-        } else {
-            newSource[sourceIndex] = _filter(newSource[sourceIndex], (card) => card.id !== sourceCards.id);
-
-            newTarget[targetIndex].push(sourceCards);
+            newTarget[targetIndex].push(sourceCard);
         }
 
         if (targetName === sourceName) {
@@ -38,31 +29,24 @@ const reducer = (prevState: AppState, action: Action): AppState => {
         }
 
         if (targetName === PileName.FOUNDATION) {
-            if (_isArray(sourceCards)) {
-                if (prevState[PileName.FOUNDATION][targetIndex].length !== ranks.indexOf(sourceCards[0].rank)) {
-                    console.info('Top most card must be of lower rank [A]');
-
-                    return prevState;
-                }
-            } else if (prevState[PileName.FOUNDATION][targetIndex].length !== ranks.indexOf(sourceCards.rank)) {
-                console.info('Top most card must be of lower rank');
+            if (prevState[PileName.FOUNDATION][targetIndex].length !== ranks.indexOf(sourceCards[0].rank)) {
+                console.info('Top most card must be of lower rank [A]');
 
                 return prevState;
             }
         }
 
-        if (targetName === PileName.TABLEAU && !_isArray(sourceCards)) {
+        if (targetName === PileName.TABLEAU && sourceCards.length > 0) {
             const topCard = _last(prevState[PileName.TABLEAU][targetIndex]);
-            console.log(prevState);
 
             if (topCard) {
-                if (topCard.color === sourceCards.color) {
+                if (topCard.color === sourceCards[0].color) {
                     console.info('Top most card must be of a different color', topCard, sourceCards);
 
                     return prevState;
                 }
 
-                if (ranks.indexOf(sourceCards.rank) !== ranks.indexOf(topCard.rank) - 1) {
+                if (ranks.indexOf(sourceCards[0].rank) !== ranks.indexOf(topCard.rank) - 1) {
                     console.info('Top most card must be of higher rank');
 
                     return prevState;
@@ -79,13 +63,12 @@ const reducer = (prevState: AppState, action: Action): AppState => {
 
     if (type === ActionTypes.REVEAL_CARD && payload.target && payload.target[2]) {
         const [targetName, targetIndex, targetCard] = payload.target;
-        console.log('REVEAL_CARD', targetName, targetCard);
 
         const newTarget = _cloneDeep(prevState[targetName]);
 
-        const card = _find(newTarget[targetIndex], (card) => card.id === targetCard.id);
-        if (card) {
-            card.isRevealed = true;
+        const cardToBeRevealed = _find(newTarget[targetIndex], (card) => card.id === targetCard.id);
+        if (cardToBeRevealed) {
+            cardToBeRevealed.isRevealed = true;
         }
 
         return {
