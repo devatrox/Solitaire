de32c5c16a5b28d7922328d2bbb66422bcf01c8f
Jason Nickel <jni@vnr.de>
2020-02-08 14:07:25 +0100

    Removed useless id on SVG

--- src/components/SvgCards.tsx
+++ src/components/SvgCards.tsx
@@ -3,6 +3,6 @@
 import { jsx, css } from '@emotion/core';
 import SvgCards from 'svg-cards/svg-cards.svg';
 
-const Svg = (): JSX.Element => (<SvgCards id="svg-cards" css={css`display: none;`} />);
+const Svg = (): JSX.Element => (<SvgCards css={css`display: none;`} />);
 
 export default Svg;
